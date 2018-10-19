097730c2e18a1af0ae6371388cadac9ae921b49b
Jayrex Acilo <ajayrexluega@gmail.com>
2018-10-20 00:08:03 +0800

    admin dashboard startup

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,15 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles('vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+mix.styles('vendor/almasaeed2010/adminlte/bower_components/font-awesome/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
+mix.styles('vendor/almasaeed2010/adminlte/bower_components/Ionicons/css/ionicons.min.css', 'public/css/ionicons.min.css');
+mix.styles('vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css', 'public/css/AdminLTE.min.css');
+mix.styles('vendor/almasaeed2010/adminlte/dist/css/skins/skin-blue.min.css', 'public/css/skins/skin-blue.min.css');
+// mix.sass('resources/sass/app.sass', 'public/css');
+
+
+mix.js('vendor/almasaeed2010/adminlte/bower_components/jquery/dist/jquery.min.js', 'public/js')
+    .js('vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js', 'public/js')
+    .js('vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js', 'public/js');
