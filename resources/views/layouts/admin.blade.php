c51ac79b3b36cb73006109f2d398c2f0ce218815
Jayrex Acilo <ajayrexluega@gmail.com>
2018-10-21 15:08:09 +0800

    admin layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,31 @@
+@include('includes.header')
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        @yield('page_header')
+        <small>@yield('page_description')</small>
+      </h1>
+      <!--<ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>-->
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+        @yield('content')
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+@include('includes.footer')
